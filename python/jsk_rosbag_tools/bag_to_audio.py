72589ba31c2059d6230cafe05b75691498a522b5
george <georgehadjiantonis@hotmail.com>
2023-04-06 12:51:38 +0200

    extract all /audio topics if topic not specified

--- python/jsk_rosbag_tools/bag_to_audio.py
+++ python/jsk_rosbag_tools/bag_to_audio.py
@@ -3,7 +3,8 @@ import os.path as osp
 import sys
 
 import numpy as np
-import rosbag, rospy
+import rosbag
+import rospy
 from scipy.io.wavfile import write as wav_write
 
 from jsk_rosbag_tools.extract import extract_oneshot_topic
@@ -12,42 +13,64 @@ from jsk_rosbag_tools.makedirs import makedirs
 
 
 def bag_to_audio(bag_filepath,
-                 wav_outpath,
-                 topic_name='/audio',
+                 wav_outpath=None,
+                 topic_name=None,
                  audio_info_topic_name=None,
-                 start_stamp=rospy.Time(0), 
+                 start_stamp=rospy.Time(0),
                  end_stamp=rospy.Time(sys.maxsize),
                  samplerate=44100,
                  channels=1,
                  overwrite=True):
+    if wav_outpath is None:
+        wav_outpath = osp.join(
+            osp.dirname(bag_filepath),
+            osp.splitext(osp.basename(bag_filepath))[0] + '.wav')
+
     if os.path.exists(wav_outpath) and overwrite is False:
         raise FileExistsError('{} file already exists.'.format(wav_outpath))
     topic_dict = get_topic_dict(bag_filepath)
-    if topic_name not in topic_dict:
-        return
 
-    audio_info_topic_name = audio_info_topic_name or topic_name + '_info'
-    # if audio_info_topic exists, extract info from it.
-    if audio_info_topic_name in topic_dict:
-        audio_info = extract_oneshot_topic(bag_filepath, audio_info_topic_name)
-        if audio_info is not None:
-            samplerate = audio_info.sample_rate
-            channels = audio_info.channels
+    all_audio_topics = []
+    if topic_name is None:
+        # Get all /audio topics
+        for t in topic_dict:
+            if t.endswith('/audio'):
+                all_audio_topics.append(t)
+    else:
+        all_audio_topics.append(topic_name)
 
-    bag = rosbag.Bag(bag_filepath)
-    audio_buffer = []
-    for _, msg, _ in bag.read_messages(topics=[topic_name], start_time=start_stamp, end_time=end_stamp):
-        if msg._type == 'audio_common_msgs/AudioData':
-            buf = np.frombuffer(msg.data, dtype='int16')
-            buf = buf.reshape(-1, channels)
-            audio_buffer.append(buf)
-    audio_buffer = np.concatenate(audio_buffer, axis=0)
+    for audio_topic in all_audio_topics:
+        if audio_topic not in topic_dict:
+            return
 
-    makedirs(osp.dirname(wav_outpath))
-    wav_write(wav_outpath, rate=samplerate, data=audio_buffer)
+        audio_info_topic_name = audio_info_topic_name or audio_topic + '_info'
+        # if audio_info_topic exists, extract info from it.
+        if audio_info_topic_name in topic_dict:
+            audio_info = extract_oneshot_topic(
+                bag_filepath, audio_info_topic_name)
+            if audio_info is not None:
+                samplerate = audio_info.sample_rate
+                channels = audio_info.channels
 
-    valid = os.stat(wav_outpath).st_size != 0
-    if valid is False:
-        return False
+        bag = rosbag.Bag(bag_filepath)
+        audio_buffer = []
+        for _, msg, _ in bag.read_messages(topics=[audio_topic], start_time=start_stamp, end_time=end_stamp):
+            if msg._type == 'audio_common_msgs/AudioData':
+                buf = np.frombuffer(msg.data, dtype='int16')
+                buf = buf.reshape(-1, channels)
+                audio_buffer.append(buf)
+        audio_buffer = np.concatenate(audio_buffer, axis=0)
+
+        # if more than one topic to be extracted create separate file names
+        if len(all_audio_topics) > 1:
+            wav_outpath_i = '{}_({}).wav'.format(wav_outpath.split(
+                '.wav')[0], audio_topic.replace('/', '_slash_'))
+
+        makedirs(osp.dirname(wav_outpath_i))
+        wav_write(wav_outpath_i, rate=samplerate, data=audio_buffer)
+
+        valid = os.stat(wav_outpath_i).st_size != 0
+        if valid is False:
+            return False
 
     return True
--- scripts/bag_to_audio.py
+++ scripts/bag_to_audio.py
@@ -2,15 +2,16 @@
 
 import argparse
 import os.path as osp
+import sys
 
+import rospy
 import termcolor
-
 from jsk_rosbag_tools.bag_to_audio import bag_to_audio
 
 
 def main():
     parser = argparse.ArgumentParser(description='rosbag to audio')
-    parser.add_argument('--out', '-o', default='',
+    parser.add_argument('--out', '-o', default=None,
                         help='output filename. '
                         'If `--audio-topic`_info is exists, '
                         "you don't have to specify samplerate and channels.")
@@ -18,26 +19,27 @@ def main():
                         default='16000')
     parser.add_argument('--channels',
                         type=int, default=1, help='number of input channels')
-    parser.add_argument('--audio-topic', type=str, default='/audio')
+    parser.add_argument('--audio-topic', type=str, default=None)
     parser.add_argument('input_bagfile')
+    parser.add_argument('--start', '-s', default=0, type=float)
+    parser.add_argument('--end', '-e', default=sys.maxsize, type=float)
     args = parser.parse_args()
 
-    if len(args.out) == 0:
-        args.out = osp.join(
-            osp.dirname(args.input_bagfile),
-            osp.splitext(osp.basename(args.input_bagfile))[0] + '.wav')
+    start_stamp = rospy.Time(args.start)
+    end_stamp = rospy.Time(args.end)
 
     audio_exists = bag_to_audio(
         args.input_bagfile, args.out,
+        start_stamp=start_stamp,
+        end_stamp=end_stamp,
         samplerate=args.samplerate,
         channels=args.channels,
         topic_name=args.audio_topic)
+
     if audio_exists is False:
-        termcolor.cprint(
-            '=> Audio topic is not exists', 'red')
+        termcolor.cprint('=> Error exporting audio', 'red')
     else:
-        termcolor.cprint(
-            '=> Audio saved to {}'.format(args.out), 'green')
+        termcolor.cprint('=> Audio files saved', 'green')
 
 
 if __name__ == '__main__':
